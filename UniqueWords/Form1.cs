e3bd35e0361c28c99860806eae04398929f5f79c
Renee <reneeq25@gmail.com>
2017-12-09 16:50:08 -0500

    alphabetical order

---
+++ UniqueWords/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniqueWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Char[] separators = new Char[] { ' ', '\r', '\n', '\t', ',', '.', ';', '!', '?', '@', '#', '$', '%', '^'};
+
+            SortedSet<String> uniqueWords =
+            new SortedSet<String>(textBox1.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries),
+                StringComparer.OrdinalIgnoreCase);
+            
+            textBox2.Text = String.Join(Environment.NewLine, uniqueWords);
+            label3.Text = uniqueWords.Count.ToString();
+
+        }
+    }
+}
